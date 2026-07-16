7341a973aca3703fe749631eebfe3a799abfcc24
pvsxxnga-max <pvsxxnga@gmail.com>
2026-07-16 12:32:42 +0700

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,1 +1,12 @@
+const mysql = require("mysql2");
 
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "elis",
+    waitForConnections: true,
+    connectionLimit: 10
+});
+
+module.exports = db.promise();
