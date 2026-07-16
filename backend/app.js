8212b003ac5339d734e176afed218c735dae3856
pvsxxnga-max <pvsxxnga@gmail.com>
2026-07-16 12:33:00 +0700

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, "../frontend")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/login.html"));
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`e-LIS Server running on http://localhost:${PORT}`);
+});
